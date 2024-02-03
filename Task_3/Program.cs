fdcb3f6137473b2e98411520e058f9ddd667d797
FelixSuzdalev <felixsuzdalev@yandex.ru>
2024-02-04 00:32:24 +0300

    g

---
+++ Task_3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task_3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите x:");
+            double x = Convert.ToDouble(Console.ReadLine()), y = 0;
+
+            if (x <= 3) y = x * x + 3 * x + 9;
+            if (x > 3) y = Math.Sin(x) / (x * x - 9);
+            Console.WriteLine("Для x = {0:f2} ", x);
+            Console.WriteLine("Результат = {0:f2}", y);
+        }
+    }
+}
---
+++ Task_3_2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task_3_2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите два любых значения для переменных x и y:");
+            double x = Convert.ToDouble(Console.ReadLine()), y = Convert.ToDouble(Console.ReadLine());
+            if (x >= y) Console.WriteLine("Значение x = {0:f2} и y = {1:f2}", x, y);
+            else
+            {
+                double z;
+                z = x; x = y; y = z;
+                Console.WriteLine("Значение x = {0:f2} и y = {1:f2}", x, y);
+            }
+        }
+    }
+}
